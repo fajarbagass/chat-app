d0fba0010f8ef8965ca079eca3a6bd01537074c3
fajarbagass <fajarbagassetiawan17@gmail.com>
2022-06-09 17:48:10 +0700

    react js & socket.io

---
+++ client/src/components/CreateUser.js
@@ -0,0 +1,14 @@
+const CreateUser = (props) => {
+  const { onCreateUser, value, onChange } = props;
+
+  return (
+    <div className="username-container">
+      <form onSubmit={onCreateUser} style={{ display: "inline-block" }}>
+        <h4 className="username-label">Create User</h4>
+        <input className="input" value={value} onChange={onChange} placeholder="enter username" />
+      </form>
+    </div>
+  );
+};
+
+export default CreateUser;
